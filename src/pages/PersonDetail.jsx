d592fec1748ba229209829ec18d131335cafea9b
Nursaadet <nursaadetozdemir@gmail.com>
2025-08-20 15:04:47 +0300

    feat: implement PersonDetail component with fetch by id

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
+const PersonDetail = () => {
+  const [person, setPerson] = useState([]);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-function PersonDetail() {
+  const getPerson = () => {
+    fetch(`https://reqres.in/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPerson(data.data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getPerson();
+  }, []);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="container text-center mt-4">
+      <img className="rounded" src={person?.avatar} alt="img" />
 
-export default PersonDetail
+      <h6>
+        {person?.first_name} {person?.last_name}
+      </h6>
+      <p>{person?.email}</p>
+      <div>
+        <button className="btn btn-danger" onClick={() => navigate("-1")}>
+          Back
+        </button>
+        <button className="btn btn-success" onClick={() => navigate("/")}>
+          Home
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PersonDetail;
